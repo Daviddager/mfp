9290927e47c5648e1f2d17745af0ddd0d4097b32
Daviddager <drdagerm@gmail.com>
2017-08-16 14:16:24 -0500

    Added connection to MFP

--- app/src/main/java/com/example/drdagermo/pushapp/MainActivity.java
+++ app/src/main/java/com/example/drdagermo/pushapp/MainActivity.java
@@ -2,15 +2,36 @@ package com.example.drdagermo.pushapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.worklight.wlclient.api.WLAccessTokenListener;
+import com.worklight.wlclient.api.WLAuthorizationManager;
 import com.worklight.wlclient.api.WLClient;
 import com.worklight.common.WLAnalytics;
+import com.worklight.wlclient.api.WLFailResponse;
+import com.worklight.wlclient.api.WLResourceRequest;
+import com.worklight.wlclient.api.WLResponse;
+import com.worklight.wlclient.api.WLResponseListener;
+import com.worklight.wlclient.auth.AccessToken;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        WLClient.createInstance(this);
+        WLClient.createInstance(this).connect(new WLResponseListener() {
+            @Override
+            public void onSuccess(WLResponse wlResponse) {
+                Log.i( "MFP", "Success" );
+            }
+
+            @Override
+            public void onFailure(WLFailResponse wlFailResponse) {
+                Log.i( "MFP", "NoConn" );
+            }
+        });
 
         WLAnalytics.init(this.getApplication());
         WLAnalytics.addDeviceEventListener(WLAnalytics.DeviceEvent.LIFECYCLE);
